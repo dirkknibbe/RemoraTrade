966127456c4b7a85ee49fccf745f7c259550e728
Dirk Knibbe <dirkknibbe@Dirks-MacBook-Pro.local>
2022-02-24 12:54:59 -0700

    ok have a basic outline and a login component started

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { NavLink, Routes, Route, useNavigate } from "react-router-dom";
+import axios from "axios";
+
+import LandingPage from "./components/LandingPage";
+import LoginForm from "./components/LoginForm";
 
 function App() {
+  // const navigate = useNavigate();
+  // const login = ({ username, password }) => {
+  //   // hit the API, obtain JWT
+  //   axios
+  //     .post(loginUrl, { username, password })
+  //     .then((res) => {})
+  //     .catch((err) => {
+  //       debugger;
+  //     });
+  // };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <LandingPage />
+        {/* <LoginForm /> */}
       </header>
     </div>
   );
---
+++ src/components/LandingPage.js
@@ -0,0 +1,21 @@
+import "../App.css";
+
+function LandingPage() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>🐋🦈</h1>
+        <h1>RemoraTrade </h1>
+        <h2>a non-custodial crypto wallet featuring...</h2>
+        <li>smart saving features</li>
+        <li>curated data insights</li>
+        <h3>Like the remora fish...</h3>
+        <h4>Learn to swim with the Whales and Sharks...</h4>
+        <h5>instead of being eaten by them...</h5>
+        <h6>👷 under construction 🚧</h6>
+      </header>
+    </div>
+  );
+}
+
+export default LandingPage;
