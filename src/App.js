1ac0d64de27a0781d26d0ef3301bd894a3b5645d
Dirk Knibbe <dirkknibbe@Dirks-MacBook-Pro.local>
2022-02-24 13:11:40 -0700

    hopefully fixes with vercel

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
+// import logo from './logo.svg';
 import "./App.css";
-import { NavLink, Routes, Route, useNavigate } from "react-router-dom";
-import axios from "axios";
+import React from "react";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import "./App.css";
+// import { NavLink, Routes, Route, useNavigate } from "react-router-dom";
+// import axios from "axios";
 
 import LandingPage from "./components/LandingPage";
-import LoginForm from "./components/LoginForm";
+// import LoginForm from "./components/LoginForm";
 
 function App() {
   // const navigate = useNavigate();
